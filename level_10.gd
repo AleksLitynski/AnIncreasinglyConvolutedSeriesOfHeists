13a35353f2e4b71aa28fdbe371eb1a58250348f7
Mike Lyle <mclyle92@gmail.com>
2021-07-26 14:32:26 -0500

    added level 10

---
+++ level_10.gd
@@ -0,0 +1,8 @@
+extends "res://level_abstract.gd"
+
+var level_id = "level_10" # must match .tscn name!
+var level_name = "Level 10"
+var level_description = """
+\"Holy shit\"
+- Jace Beleren
+"""
--- level_manager.gd
+++ level_manager.gd
@@ -6,7 +6,7 @@ func _ready():
 	rand = RandomNumberGenerator.new()
 	rand.randomize()
 	# load_level("main_menu", false)
-	load_level("level_9", false)
+	load_level("level_10", false)
 	play_next_song()
 
 var current_level = null
